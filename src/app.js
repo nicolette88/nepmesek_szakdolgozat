ac961d4d9b053f594ce9c7a1e02646e7ae20ae35
Niki <nicolette88@gmail.com>
2022-01-13 16:32:27 +0100

    második commit

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import html from './index.html';
+import './style.css';
+// import './style.scss';
+
+// async function getImage() {
+//   console.log('hello');
+//   const response = await fetch('https://dog.ceo/api/breeds/image/random');
+//   const data = await response.json();
+//   console.log(data.message);
+
+// }
+
+// getImage();
+
+// document.getElementById('demo').onclick = function () {
+//   location.href = './nepmesek.html';
+// };
